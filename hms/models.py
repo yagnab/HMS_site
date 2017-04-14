eaddede12feb0287a7110a8e8aba47fa568dec04
Yagna <yagnab@hotmail.com>
2017-04-14 21:53:02 +0100

    removed the random func to test adding to branch in another way

--- hms/models.py
+++ hms/models.py
@@ -29,9 +29,6 @@ class Room(models.Model):
     def __str__(self):
         return "Room: " + str(self.room_number)
 
-    def random_func(self):
-        return "wut is this lmao"
-
 ##class Bed(models.Model):
 ##    #relations
 ##    room = models.ManyToManyField(Room)
